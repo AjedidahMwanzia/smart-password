8d731990c5ad3a01ff4469f830ee12f190d8544b
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-04-22 17:28:30 +0300

    create save user details method

--- user.py
+++ user.py
@@ -3,12 +3,15 @@ class User:
     Class that generates new instances of user-details
     """
 user_details = []
+
+def __init__(self,username,password):
+    
+        self.username = username
+        self.password = password
 '''
 __init__ method that helps us define properties for our objects.
 '''
-def __init__(self,first_name,last_name,phone_number,email):
-        self.first_name = first_name
-        self.last_name =last_name
-        self.phone_number =phone_number
-        self.email= email
+def save_user_details(self):
+    User.user_details.append(self) 
+     
 
